d14c90104fc857628c7ed4fd210d12fac2232c12
sadaiyer <iyersada@gmail.com>
2020-05-10 14:07:10 -0600

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -5,5 +5,5 @@ alias kgp='kubectl get pods'
 alias kgs='kubect get services'
 alias kgd='kubectl get deploy'
 alias c=clear
-alias rp='kubectl get pod; read -p "Filename:" podFileName;  kubectl replace -f $podFileName --force --grace-period=0; kubectl get pod'
-alias dp='kubectl get pod; read -p "Podname:" podName;  kubectl delete pod $podName --force --grace-period=0; kubectl get pod'
+alias rp='kubectl get pod; read -p "Filename:" podFileName;  kubectl replace -f $podFileName --force --grace-period=0; kubectl get pod -watch'
+alias dp='kubectl get pod; read -p "Podname:" podName;  kubectl delete pod $podName --force --grace-period=0; kubectl get pod -watch'
