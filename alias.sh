63441c931266eb271458c9728bd429928bd43b7b
Sada Iyer <iyersada@gmail.com>
2020-06-01 18:48:25 -0600

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -27,7 +27,15 @@ alias kds='kubectl describe service'
 alias kdn='kubectl describe node'
 
 alias kaf='kubectl apply -f '
+
+alias kgp='kubectl get pods'
+alias kgs='kubect get services'
+alias kgd='kubectl get deploy'
 alias kcf='kubectl create -f '
+alias sc='kubectl get ns;read -p "Enter:" nameSpace;kubectl config set-context --current --namespace $nameSpace;k config view | grep -i namespace'
+alias rp='kubectl get pod;read -p "Filename:" podFileName;kubectl replace -f $podFileName --force --grace-period=0;kubectl get pod'
+alias dp='kubectl get pod; read -p "Podname:" podName;kubectl delete pod $podName --force --grace-period=0; kubectl get pod'
+
 
 alias kcv='kubectl config view'
 alias kcc='kubectl config current-context'
