9bbd86f4d03ae86c4c7c3a9c7e7d179eceab158b
Sada Iyer <iyersada@gmail.com>
2020-06-01 18:51:31 -0600

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -31,6 +31,8 @@ alias kaf='kubectl apply -f '
 alias kcv='kubectl config view'
 alias kcc='kubectl config current-context'
 alias kcs='kubectl config set-context'
+alias w='watch -n 0.5 "kubectl config current-context; echo ''; kubectl config view | grep namespace; echo ''; kubectl get namespace,node,ingress,pod,svc,job,cronjob,deployment,rs,pv,pvc,secret,ep -o wide"'
+
 
 #minimum set of alias
 alias kgp='kubectl get pods'
@@ -40,8 +42,7 @@ alias kcf='kubectl create -f '
 alias sc='kubectl get ns;read -p "Enter:" nameSpace;kubectl config set-context --current --namespace $nameSpace;k config view | grep -i namespace'
 alias rp='kubectl get pod;read -p "Filename:" podFileName;kubectl replace -f $podFileName --force --grace-period=0;kubectl get pod'
 alias dp='kubectl get pod; read -p "Podname:" podName;kubectl delete pod $podName --force --grace-period=0; kubectl get pod'
+export dr='--dry-run -o yaml'
 
 
 
-
-alias w='watch -n 0.5 "kubectl config current-context; echo ''; kubectl config view | grep namespace; echo ''; kubectl get namespace,node,ingress,pod,svc,job,cronjob,deployment,rs,pv,pvc,secret,ep -o wide"'
