6367dba778d65036e854c2f80a9286cf480f1df9
Sada Iyer <iyersada@gmail.com>
2023-12-09 11:46:14 -0700

    Update alias.sh
    Dec 2023 update

--- alias.sh
+++ alias.sh
@@ -1,76 +1,73 @@
+#ALIAS# 
+# source alias.sh
+
 set -o vi
-
-alias k=kubectl
-alias kubect=kubectl
-#alias sada='k run -it pod1 --image=cosmintitei/bash-curl --restart=Never --rm'
-alias bb='k run bb --image=busybox:1.28 --restart=Never --rm -it -- /bin/sh'
-alias sada='k run -it pod1 --image=sadaiyer/bash-curl --restart=Never --rm'
-alias kgp='kubectl get pods'
-alias kgs='kubect get services'
-alias kgd='kubectl get deploy'
-
-alias kgpw='kubectl get pods -o wide'
-alias kgsw='kubect get services -o wide'
-alias kgdw='kubectl get deploy -o wide'
-
-alias c=clear
 alias ll='ls -ltr'
 
-alias kw='kubect get pods --watch'
+## echo 'autocmd FileType yaml setlocal ts=2 sts=2 sw=2 et nonu' >> ~/.vimrc
+
+export ETCDCTL_API=3
+
+alias ll='ls -ltr'alias k='kubectl'
+alias kgp='k get pods'
+alias kgd='k get deploy'
+alias kgs='k get svc'
+alias kcf='k create -f'
+alias dp='k get pods;read -p "Enter podName:" podName;k delete pod $podName --force --grace-period=0;k get pods'
+alias sc='k get ns;read -p "Enter nSpace:" nSpace;k config set-context --current --namespace=$nSpace;k config view --minify | grep namespace;export PS1="\[\]\u:\H:$nSpace:$ "'
+alias c='clear'
+export dr='--dry-run=client -o yaml'
+alias mycurl=‘kubectl run mycurlpod --image=curlimages/curl -i --tty -- sh’
+
+# https://bash-prompt.net/guides/create-system-load/
+# Memory Section
+# To generate memory load, 
+alias showmem='grep Mem /proc/meminfo'
+echo MemFree='MemFree is the total amount of unused physical memory (RAM)'
+echo MemAvailable='MemAvailable is the estimated amount of RAM usable for the system and new app to start without caches, it is MemFree + reclaimable memory blocks like those inactive pages, etc.'
+
+# CPU Section
+# To generate CPU load, use STRESS
+# nohup stress --cpu 8 --vm-bytes $(awk '/MemAvailable/{printf "%d\n", $2 * 0.9;}' < /proc/meminfo)k --vm-keep -m 1 &
 
 
-alias rp='kubectl get pod;read -p "Filename:" podFileName;kubectl replace -f $podFileName --force --grace-period=0;kubectl get pod'
-alias dp='kubectl get pod; read -p "Podname:" podName;kubectl delete pod $podName --force --grace-period=0; kubectl get pod'
+alias showcpu='grep proc /proc/cpuinfo'
+alias showos='lsb_release -a'
 
+alias showphysicalcpus='grep "physical id" /proc/cpuinfo | sort | uniq | wc -l'
+alias showvirtualprocessors='grep "^processor" /proc/cpuinfo | wc -l'
+alias shownumcores='grep "cpu cores" /proc/cpuinfo | uniq'
+
+
+
+#cni
+alias cni='kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml'
+
+
+
+export EDITOR=vi
+export KUBE_EDITOR=vi
+
+export VISUAL=vim
+export EDITOR="$VISUAL"
 
 alias kdp='kubectl describe pod'
+alias kgn='kubectl get nodes'
 alias kdd='kubectl describe deployment'
 alias kds='kubectl describe service'
 alias kdn='kubectl describe node'
-
 alias kaf='kubectl apply -f '
-
 alias kcv='kubectl config view'
 alias kcc='kubectl config current-context'
 alias kcs='kubectl config set-context'
 alias w='watch -n 0.5 "kubectl config current-context; echo ''; kubectl config view | grep namespace; echo ''; kubectl get namespace,node,ingress,pod,svc,job,cronjob,deployment,rs,pv,pvc,secret,ep -o wide"'
 
 
-#minimum set of alias
-alias kgp='kubectl get pods'
-alias kgs='kubect get services'
-alias kgd='kubectl get deploy'
-alias kcf='kubectl create -f '
-alias sc='kubectl get ns;read -p "Enter:" nameSpace;kubectl config set-context --current --namespace $nameSpace;k config view | grep -i namespace'
-alias sc='k get ns;read -p "EnterNS:" nSpace;k config set-context --current --namespace=$nSpace;k config view --minify | grep -i namespace;export NS=`k config view|grep namespace | cut -f2 -d:`;export PS1="[\u@\h@$NS\W]\$"'
-alias rp='kubectl get pod;read -p "Filename:" podFileName;kubectl replace -f $podFileName --force --grace-period=0;kubectl get pod'
-alias dp='kubectl get pod; read -p "Podname:" podName;kubectl delete pod $podName --force --grace-period=0; kubectl get pod'
-export dr='--dry-run=client -o yaml'
-export aa="grep -i '#' -A 2"
-export vimrc='autocmd FileType yaml setlocal et ts=2 ai sw=2 nu sts=0'
-export ETCDCTL_API=3
-
-alias showmem='grep Mem /proc/meminfo'
-alias showcpu='grep proc /proc/cpuinfo'
-alias showos='lsb_release -a'
-alias cni='kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"'
-
-alias bb='k run bb --image=busybox:1.28 --restart=Never --rm -it -- /bin/sh -c "sleep 3600"'
-alias ac='k run ac --image=sadaiyer/alpine-curl --restart=Never --rm -it '
-
-export EDITOR=vi
-export KUBE_EDITOR=vi
-export VISUAL=vim
-export EDITOR="$VISUAL"
-
-alias kgn='kubectl get nodes'
-alias kdp='kubectl describe pod '
-alias wn='kubectl config view | grep namespace'
+alias wn='kubectl config view --minify | grep namespace'
 export KN=' -n kube-system'
 alias kw='kgp -o wide -w'
 alias kj='kubeadm token create --print-join-command'
 alias kh='kubectl get cs;kubectl cluster-info;kubectl version --short; kubeadm alpha certs check-expiration'
-
 alias pa='dpkg --print-architecture'
 alias getsa='k get clusterrolebinding -o custom-columns=CRB:.metadata.name,CR:.roleRef.name,SA:.subjects[*].name | grep -v system'
 alias kgcc='kgp -o custom-columns=NAME:.metadata.name,IMAGE:.spec.containers[*].image'
@@ -83,5 +80,8 @@ alias kdnp='k delete networkpolicy '
 alias ekm='cd /etc/kubernetes/manifests/'
 alias kal='cd /var/log/pods/kube-system_kube-apiserver*/kube-apiserver/'
 
-c
 
+alias mcurl='kubectl run --rm utils -it --image arunvelsriram/utils bash'
+alias bb='k run bb --image=busybox:1.28 --restart=Never --rm -it -- /bin/sh -c "sleep 3600"'
+
+c
